03bd98e9c93394016372d645673b2321e56a5661
aravind167-y <2311it010167@mallareddyuniversity.ac.in>
2026-09-08 11:34:29 +0530

    changes made

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+        <h1>Logo</h1>
+        <ul>
+           
+            <li>
+                <Link to='/login'>Login</Link>
+            </li>
+            <li>
+                <Link to='/signup'>Signup</Link>
+            </li>
+        </ul>
+    </div>
   )
 }
 
